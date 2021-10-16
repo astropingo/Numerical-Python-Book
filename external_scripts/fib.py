52d6b8a3be837eff7fb29ade48b469b778e8b29c
mvpetri <mvpetri@gmail.com>
2021-10-16 13:41:39 -0300

    first chapter

---
+++ external_scripts/fib.py
@@ -0,0 +1,14 @@
+def fib(n):
+    """
+    Return a list of the first Fibonacci numbers.
+    """
+    f0, f1 = 0, 1
+    f = [1] * n
+
+    for i in range(1, n):
+        f[i] = f0 + f1
+        f0, f1 = f1, f[i]
+    
+    return f
+
+print(fib(10))
